8310adf4fa0854a6bbd236bafc88661af296af07
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-08-15 11:08:44 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,25 +1,40 @@
-# Example Terraform module
+<!-- BEGIN_TF_DOCS -->
+# Fortigate BGP configuration module
 
-Terraform module which is an example.
-
-
-## Usage
-```hcl
-module "example" {
-  source        = "github.com/sncs-uk/example-module"
-  parameter     = "value"
-  do_something  = true
-}
-```
+This terraform module configures BGP on a firewall
 
 ## Requirements
-| Name | Version |
-|------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0 |
-| <a name="requirement_example"></a> [example](#requirement\_example) | >= 1.23 |
+
+No requirements.
 
 ## Providers
 
 | Name | Version |
 |------|---------|
-| <a name="provider_example"></a> [example](#provider\_example) | >= 1.23 |
+| <a name="provider_fortios"></a> [fortios](#provider\_fortios) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [fortios_router_bgp.router_bgp](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/router_bgp) | resource |
+| [fortios_router_prefixlist.prefixlist_v4](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/router_prefixlist) | resource |
+| [fortios_router_prefixlist6.prefixlist_v6](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/router_prefixlist6) | resource |
+| [fortios_router_routemap.routemaps](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/router_routemap) | resource |
+| [fortios_routerbgp_neighbor.peer](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/routerbgp_neighbor) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_config_path"></a> [config\_path](#input\_config\_path) | Path to base configuration directory | `string` | n/a | yes |
+| <a name="input_vdoms"></a> [vdoms](#input\_vdoms) | List of VDOMs from which to pull in configuration | `list(string)` | `[]` | no |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ main.tf
@@ -0,0 +1,122 @@
+/**
+ * # Fortigate BGP configuration module
+ *
+ * This terraform module configures BGP on a firewall
+ */
+terraform {
+  required_providers {
+    fortios = {
+      source  = "fortinetdev/fortios"
+    }
+  }
+}
+locals {
+  vdom_bgp_yaml = {
+    for vdom in var.vdoms : vdom => yamldecode(file("${var.config_path}/config/${vdom}/bgp.yaml")) if fileexists("${var.config_path}/config/${vdom}/bgp.yaml")
+  }
+  bgp_peers_yaml        = { for vdom in var.vdoms : vdom => try(local.vdom_bgp_yaml[vdom].peers, []) }
+  prefix_lists_yaml     = { for vdom in var.vdoms : vdom => try(local.vdom_bgp_yaml[vdom].prefix_lists, []) }
+  route_maps_yaml     = { for vdom in var.vdoms : vdom => try(local.vdom_bgp_yaml[vdom].route_maps, []) }
+
+  bgp_peers     = flatten([
+    for vdom in var.vdoms : [
+      for peer in local.bgp_peers_yaml[vdom] : [ merge(peer, { vdom = vdom }) ]
+    ]
+  ])
+  route_maps    = flatten([
+    for vdom in var.vdoms : [
+      for map in local.route_maps_yaml[vdom] : [ merge(map, { vdom = vdom }) ]
+    ]
+  ])
+
+  prefix_lists_v4     = flatten([
+    for vdom in keys(local.prefix_lists_yaml) : [
+      for prefixlist in local.prefix_lists_yaml[vdom] : [ merge(prefixlist, { vdom = vdom })] if try(prefixlist.address_family, "") == "ipv4"
+    ]
+  ])
+
+  prefix_lists_v6     = flatten([
+    for vdom in keys(local.prefix_lists_yaml) : [
+      for prefixlist in local.prefix_lists_yaml[vdom] : [ merge(prefixlist, { vdom = vdom })] if try(prefixlist.address_family, "") == "ipv6"
+    ]
+  ])
+}
+
+resource fortios_router_bgp router_bgp {
+  for_each      = local.vdom_bgp_yaml
+  as_string     = each.value.asn
+  router_id     = each.value.router_id
+  vdomparam     = each.key
+}
+
+resource fortios_router_prefixlist6 prefixlist_v6 {
+  for_each                      = { for prefix_list in local.prefix_lists_v6 : prefix_list.name => prefix_list}
+  name                          = each.key
+  dynamic rule {
+    for_each = { for rule in each.value.rules : rule.id => rule }
+    content {
+      id      = rule.value.id
+      action  = rule.value.action
+      prefix6 = try(rule.value.prefix, null)
+      ge      = try(rule.value.ge, null)
+      le      = try(rule.value.le, null)
+      flags   = try(rule.value.flags, null)
+    }
+  }
+  vdomparam                     = each.value.vdom
+}
+
+resource fortios_router_prefixlist prefixlist_v4 {
+  for_each                      = { for prefix_list in local.prefix_lists_v4 : prefix_list.name => prefix_list}
+  name                          = each.key
+  dynamic rule {
+    for_each = { for rule in each.value.rules : rule.id => rule }
+    content {
+      id      = rule.value.id
+      action  = rule.value.action
+      prefix  = try(rule.value.prefix, null)
+      ge      = try(rule.value.ge, null)
+      le      = try(rule.value.le, null)
+      flags   = try(rule.value.flags, null)
+    }
+  }
+  vdomparam                     = each.value.vdom
+}
+
+resource fortios_router_routemap routemaps {
+  depends_on  = [
+    fortios_router_prefixlist.prefixlist_v4,
+    fortios_router_prefixlist6.prefixlist_v6,
+  ]
+  for_each                      = { for route_map in local.route_maps : route_map.name => route_map}
+  name                          = each.key
+  dynamic rule {
+    for_each = { for rule in each.value.rules : rule.id => rule }
+    content {
+      id                  = rule.value.id
+      action              = rule.value.action
+      match_ip_address    = try(rule.value.match_ip_address, null)
+      match_ip6_address   = try(rule.value.match_ip6_address, null)
+      match_flags         = try(rule.value.match_flags, null)
+      set_flags           = try(rule.value.set_flags, null)
+    }
+  }
+  vdomparam                     = each.value.vdom
+}
+
+resource fortios_routerbgp_neighbor peer {
+  for_each                      = { for peer in local.bgp_peers : peer.ip => peer}
+  depends_on                    = [ fortios_router_routemap.routemaps ]
+  ip                            = each.value.ip
+  remote_as                     = each.value.remote_as
+  update_source                 = try(each.value.update_source, null)
+  vdomparam                     = each.value.vdom
+  activate                      = try(each.value.activate, "disable")
+  activate6                     = try(each.value.activate6, "disable")
+  capability_default_originate  = try(each.value.capability_default_originate, "disable")
+  capability_default_originate6 = try(each.value.capability_default_originate6, "disable")
+  prefix_list_out               = try(each.value.prefix_list_out, null)
+  prefix_list_out6              = try(each.value.prefix_list_out6, null)
+  route_map_out                 = try(each.value.route_map_out, null)
+  route_map_out6                = try(each.value.route_map_out6, null)
+}
